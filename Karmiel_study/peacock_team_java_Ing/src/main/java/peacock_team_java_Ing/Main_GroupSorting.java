9f4f329a84bc1ed5b6bb52cc4d43487c7d6f3114
mkreiniuk <mkreiniuk@gmail.com>
2024-01-16 23:18:29 +0200

    Algorithm has been reworked

--- Karmiel_study/peacock_team_java_Ing/src/main/java/peacock_team_java_Ing/Main_GroupSorting.java
+++ Karmiel_study/peacock_team_java_Ing/src/main/java/peacock_team_java_Ing/Main_GroupSorting.java
@@ -2,8 +2,10 @@ package peacock_team_java_Ing;
 
 
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.file.Files;
@@ -12,13 +14,16 @@ import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
@@ -26,7 +31,7 @@ public class Main_GroupSorting {
 	
 	
 	
-	private final static String REGEX="^(\"\\d*\")(;\"\\d*\")*$";
+	
 	private static String FILEPATH= "";
 	
 
@@ -34,24 +39,92 @@ public class Main_GroupSorting {
 		long start = System.currentTimeMillis();
 		FILEPATH = args[0];
 		
-		Set<Long[]> dataRead = readNumbersFromFile(FILEPATH);
+		matchingAndGrouping(FILEPATH);
 		
-		
-		if(dataRead.isEmpty()) {
-			System.out.println("Данные не найдены");
-		}
-		
-		Set<Map<Long, Set<Long[]>>> unGroupMatches= findMatches(dataRead);
-	
-		List<Set<Long[]>> groups= groupMatches(unGroupMatches);
-		
-		print(groups);
 		System.out.printf("Общее время выполнения команды: %.3f секунд ",(System.currentTimeMillis()- start)*0.001);
 
 
 	}
 	
-	private static void print(List<Set<Long[]>> groups) {
+	private static void matchingAndGrouping(String path) {
+		   BufferedReader reader;
+		
+		   
+		
+		  try {
+			reader = new BufferedReader(new FileReader(path));
+			 List<Set<String>> groups = new ArrayList<>();
+			 List<Map<String, Integer>> connections = new ArrayList<>();
+
+	          String line = reader.readLine();
+	          while(line != null) {
+	        	  String [] row = getRows(line);
+	        	  Integer groupNumber = null; 
+	        	  for (int i = 0; i < Math.min(connections.size(), row.length); i++) {
+	                    Integer groupNumber2 = connections.get(i).get(row[i]);
+	                    if (groupNumber2 != null) {
+	                        if (groupNumber == null) {
+	                            groupNumber = groupNumber2;
+	                        } else if (!Objects.equals(groupNumber, groupNumber2)) {
+	                            for (String line2 : groups.get(groupNumber2)) {
+	                                groups.get(groupNumber).add(line2);
+	                                apply(getRows(line2), groupNumber, connections);
+	                            }
+	                            groups.set(groupNumber2, new HashSet<>());
+	                        }
+	                    }
+	                }
+	        	  if (groupNumber == null) {
+	                    if (Arrays.stream(row).anyMatch(s -> !s.isEmpty())) {
+	                        groups.add(new HashSet<>(List.of(line)));
+	                        apply(row, groups.size() - 1, connections);
+	                    }
+	                } else {
+	                    groups.get(groupNumber).add(line);
+	                    apply(row, groupNumber, connections);
+	                }
+	                line = reader.readLine();
+	        		 	
+				}
+	          reader.close();
+	          
+	          print(groups);
+	        	  
+	          
+		} catch (Exception e) {
+			System.out.println("Ошибка чтения файла");
+			e.printStackTrace();
+		}
+         
+		
+	}
+
+	private static void apply(String[] rows, Integer groupNumber, List<Map<String, Integer>> connections) {
+		 for (int i = 0; i < rows.length; i++) {
+	            if (rows[i].isEmpty()) {
+	                continue;
+	            }
+	            if (i < connections.size()) {
+	                connections.get(i).put(rows[i], groupNumber);
+	            } else {
+	                HashMap<String, Integer> map = new HashMap<>();
+	                map.put(rows[i], groupNumber);
+	                connections.add(map);
+	            }
+	        }
+		
+	}
+
+	private static String[] getRows(String line) {
+		 for (int i = 1; i < line.length() - 1; i++) {
+	            if (line.charAt(i) == '"' && line.charAt(i - 1) != ';' && line.charAt(i + 1) != ';') {
+	                return new String[0];
+	            }
+	        }
+	        return line.replaceAll("\"", "").split(";");
+	}
+
+	private static void print(List<Set<String>> groups) {
 		
 		String answerPath = FILEPATH.replace(".txt", "-result.txt");
 		Path outPath = Path.of(answerPath);
@@ -65,20 +138,20 @@ public class Main_GroupSorting {
 		
 		try (FileOutputStream fos = new FileOutputStream(outPath.toFile());
 	             PrintStream out = new PrintStream(fos)) {
+			long groupNum= groups.stream().filter(s -> s.size() > 1).count();
 			
-			 out.println("Ощеее количество групп: " + groups.stream().count());
+			 out.println("Ощеее количество групп: " + groupNum);
 	         groups.sort(Comparator.comparingLong(s -> -s.size()));
 	         int i = 0;
-	         for (Set<Long[]> group : groups) {
+	         for (Set<String> group : groups) {
 	             i++;
-	             out.println("\nГруппа " + i);
-	             for (Long[] line : group) {
-	             for(Long l : line) {
-	            	 out.print(l+" ");
-	            	
-	             }
-	            out.println();
-	             }
+	             
+	             if (i<= groupNum) {
+					out.println("\nГруппа " + i);
+					for (String line : group) {
+						out.println(line);
+					} 
+				}
 	         }
 
 	        } catch (FileNotFoundException e) {
@@ -93,110 +166,6 @@ public class Main_GroupSorting {
 	
     
 		
-	}
-
-	private static List<Set<Long[]>> groupMatches(Set<Map<Long, Set<Long[]>>> unGroupMatches) {
-		List<Set<Long[]>> groups = new ArrayList<>();
-		
-		for(Map<Long, Set<Long[]>> map: unGroupMatches) {
-			for(Long key: map.keySet()) {
-				Set<Long[]> temp = map.get(key);
-				boolean isAdded = false;
-					for(Set<Long[]> group: groups) {
-						for(Long[] line: temp) {
-							if(group.contains(line)) {
-								group.addAll(temp);
-								isAdded= true;
-								break;
-							}
-						}
-					}
-				if(!isAdded) {
-					groups.add(temp);
-				}
-					
-				
-			}
-		}
-		
-		
-		return groups;
-	}
-
-	private static Set<Map<Long, Set<Long[]>>> findMatches(Set<Long[]> dataRead) {
-		Set<Map<Long, Set<Long[]>>> matches = new HashSet<>();
-		int maxSize = longestLine(dataRead);
-		int size = 0;
-		
-		
-		while(size <= maxSize) {
-			int point= size;
-			long[] temp = dataRead.stream()
-					.flatMapToLong(l -> LongStream.of(Arrays.stream(l)
-							.skip(point).findFirst().orElse(0L)))
-					.toArray();
-			
-			   Set<Long> clear = new HashSet<>();
-	            Set<Long> copies = new HashSet<>();
-	            for (long l : temp) {
-	                if (l != 0 && !clear.add(l)) {
-	                    copies.add(l);
-	                }
-	            }
-
-	            Map<Long, Set<Long[]>> mapWithMatches = dataRead.stream()
-	                    .filter(l -> Arrays.stream(l)
-	                            .skip(point)
-	                            .limit(1)
-	                            .anyMatch(copies::contains))
-	                    .collect(Collectors.groupingBy(
-	                            s -> Arrays.stream(s)
-	                                    .skip(point)
-	                                    .limit(1)
-	                                    .findFirst().orElse(0L),
-	                            Collectors.toSet()
-	                    ));
-	            matches.add(mapWithMatches);
-	            size++;
-		}
-		return matches;
-	}
-
-	private static int longestLine(Set<Long[]> dataRead) {
-		
-		return dataRead.stream().mapToInt(d ->d.length).max().orElse(0);
-	}
-
-	private static Set<Long[]> readNumbersFromFile(String filePath) {
-		 Set<Long[]> numberSet = new HashSet<>();
-		 Pattern pattern = Pattern.compile(REGEX);
-		try(Stream<String> lines = Files.lines(Paths.get(filePath))) {
-			
-			
-			numberSet= lines.filter(l ->{
-				Matcher matcher = pattern.matcher(l);
-                return matcher.matches();
-			})	.filter(l -> l.length()>2)
-					.distinct()
-					.map(l -> l.replace("\"\"", "0"))
-                    .map(l -> l.replace("\"", ""))
-                    .map(l->l.trim())
-                    .map(l -> Arrays.stream(l.split(";"))
-                    		.map(Long::valueOf)
-                    		.toList()
-                    		.toArray(Long[]::new))
-                    .collect(Collectors.toSet());                                 
-					
-		} catch (IOException  e ) {
-			
-			e.printStackTrace();
-	System.err.println("Ошибка чтения файла");
-		}catch(NumberFormatException e ) {
-			e.printStackTrace();
-			System.err.println("Ошибка преобразования файла");
-		}
-		
-		return numberSet;
 	}
 
 
